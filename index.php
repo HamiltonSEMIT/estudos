e48e7083a28d1e55427b0184d07012e92892d846
Hamiltonspinola <hamiltonmoraes64@gmail.com>
2022-06-08 22:20:00 -0300

    Propriedade transitions

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Transitions</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <nav class="nav-menu">
+            <ul class="lista-menu">
+                <li class="item-menu">Home</li>
+                <li class="item-menu">Sobre</li>
+                <li class="item-menu">Contato</li>
+                <li class="item-menu">Produtos</li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
